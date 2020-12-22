e797469e6ddad52b5ea10a7bc068797c364f5095
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-22 13:21:49 +0000

    Update menuRoutes.js

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,1 +1,26 @@
+import express from "express"
+import {menus, PurchaseImg} from "../data/menus.js"
 
+const router = express.Router()
+
+router.get("/purchase", (req, res) => {
+    res.json(PurchaseImg)
+}) 
+
+router.get("/menus", (req, res) => {
+    res.json(menus)
+})
+
+router.get("/menus/:id", (req, res) => {
+    const menu = menus.find((m) => m._id === req.params.id)
+  
+    res.json(menu)
+})
+
+// app.get("/api/menus/:id", (req, res) => {
+//     const menu = menus.find((m) => m._id ===req.params.id)
+  
+//     res.json(menu)
+//   })
+  
+export default router
