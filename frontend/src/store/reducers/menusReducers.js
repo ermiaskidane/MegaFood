3645d47d81d1334f142a9470612dc7e4895c606e
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-23 16:47:14 +0000

    Update menusReducers.js

--- frontend/src/store/reducers/menusReducers.js
+++ frontend/src/store/reducers/menusReducers.js
@@ -1,1 +1,44 @@
+import { 
+    MENUS_FOOD_REQUEST,
+    MENUS_FOOD_SUCCESS,
+    MENUS_FOOD_FAIL,
+    MENU_ADD_ITEM,
+    MENU_REMOVE_ITEM
+} from "../constants/menusConstants"
 
+export const menusDetailReducer = (state = {menusItem: []}, action) => {
+    switch (action.type) {
+        case MENU_ADD_ITEM:
+            const item = action.payload
+
+            const existItems = state.menusItem.find((x) => x.menu === item.menu)
+
+            if(existItems) {
+                return {
+                    ...state,
+                    menusItem: state.menusItem.map((x) => x.menu === existItems.menu ? item : x)
+                }
+            } else {
+                return {
+                    ...state,
+                    menusItem: [...state.menusItem, item]
+                }
+            }
+            case MENU_REMOVE_ITEM:
+                return {
+                    ...state,
+                    menusItem: state.cartItems.filter((x) => x.menu !== action.payload)
+                }
+            default:
+                return state
+        
+        // case MENUS_FOOD_REQUEST:
+        //     return {loading: true}
+        // case MENUS_FOOD_SUCCESS:
+        //     return {loading: false, menu: action.payload}
+        // case MENUS_FOOD_FAIL:
+        //     return { loading: false, error: action.payload}
+        // default:
+        //     return state
+    }
+}
