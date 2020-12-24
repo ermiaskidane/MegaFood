5e13b032c1aa1f61e8a16b922878d9c50e4f81dc
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-24 15:59:19 +0000

    Update navShopcart.js

--- frontend/src/components/navigation/navigationIconPages/navShopcart/navShopcart.js
+++ frontend/src/components/navigation/navigationIconPages/navShopcart/navShopcart.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import {useDispatch, useSelector} from "react-redux";
 import { NavLink } from "react-router-dom";
 import "./navShopcart.scss";
 
