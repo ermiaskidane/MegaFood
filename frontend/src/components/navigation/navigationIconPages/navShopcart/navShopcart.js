2db2590a0e5bd0f88ffb4a6f6c4703d4395f853d
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-24 16:01:01 +0000

    Update navShopcart.js

--- frontend/src/components/navigation/navigationIconPages/navShopcart/navShopcart.js
+++ frontend/src/components/navigation/navigationIconPages/navShopcart/navShopcart.js
@@ -24,12 +24,34 @@ const NavShopcart = props => {
           </svg>
         </div>
 
-        <div className="navShopcart__detail--content">
+      {menusItem.length !== 0 ? 
+    (menusItem.map((item) => 
+    (<div className="navShopcart__detail--menu">
+      <ul key={item.menu}>
+      <li>
+        <figure>
+          <NavLink to="/">
+            <img src={item.image} alt={item.name} 
+              style={{ 
+                height: "3rem",
+                width: "2.8rem",
+                borderRadius: "50%" }}
+            />
+          </NavLink>
+        </figure>
+        <p className="Name"><NavLink to="/">{item.name}</NavLink></p>
+        <p className="price">{item.price}</p>
+        <p className="offer">{item.offer}</p>
+        </li>
+    </ul>
+    </div>
+      )))
+    : (<div className="navShopcart__detail--content">
           <h2>Your Shopping Bag is Empty</h2>
           <button>
             <NavLink to="/shop">Go to The Shop</NavLink>
           </button>
-        </div>
+        </div>)}
       </div>
     </div>
   );
