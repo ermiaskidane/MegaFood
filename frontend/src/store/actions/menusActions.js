e119d78b85be43fe97a4721b10cc317d1e05b2c2
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-30 17:19:02 +0000

    Update menusActions.js

--- frontend/src/store/actions/menusActions.js
+++ frontend/src/store/actions/menusActions.js
@@ -35,31 +35,3 @@ import {
   
     localStorage.setItem("menusItem", JSON.stringify(getState().menusDetail.menusItem))
   }
- 
-//   export const detailMenus = (id) => async(dispatch)  => {
-//     try {
-//         dispatch({
-//             type: MENUS_FOOD_REQUEST
-//         })
-
-//         const { data } = await axios.get(`/api/homeScreen/menus/${id}`)
-
-//         dispatch({
-//             type: MENUS_FOOD_SUCCESS,
-//             payload: {
-//                 menu: data._id,
-//                 name: data.name,
-//                 image: data.image,
-//                 price: data.price,
-//                 oldPrice: data.oldPrice,
-//                 offer: data.offer,
-//                 shopcart: true
-//             }
-//         }) 
-//     } catch(error){
-//         dispatch({
-//             type: MENUS_FOOD_FAIL,
-//               payload: error.response && error.response.data.message ? error.response.data.message : error.message
-//         })
-//     }
-//   }
