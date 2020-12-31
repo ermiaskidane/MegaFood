f0bd52157b369be87fd88a45116c67be2bb1ac74
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-31 12:50:16 +0000

    Update menu.js

--- frontend/src/components/menu/menu.js
+++ frontend/src/components/menu/menu.js
@@ -69,7 +69,7 @@ const Menu = () => {
              </div>
            </div>
            {/* ##### purchase Content ##### */}
-          <p onClick={() => addToCartHandler(menu._id)}>{menu.name}</p>
+          <p onClick={() => addToCartHandler(menu._id, qty)}>{menu.name}</p>
           <p>
             {
               menu.oldPrice ? (<s>${menu.oldPrice}</s>) : null}
