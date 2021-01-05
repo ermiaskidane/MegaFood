6f5d3ac8d6569e6e94e8b5bd251e8edb621f99ab
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-01-05 16:52:27 +0000

    Update ShopMenuModel.js

--- backend/models/ShopMenuModel.js
+++ backend/models/ShopMenuModel.js
@@ -1,1 +1,42 @@
+import mongoose from "mongoose"
 
+const ShopMenuSchema = mongoose.Schema(
+    {
+        // user: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        //     required: true,
+        //     ref: "User"
+        // },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        oldPrice: {
+            type: Number,
+            default: 0,
+        },
+        offer: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const ShopMenu = mongoose.model("ShopMenu", ShopMenuSchema)
+
+export default ShopMenu
