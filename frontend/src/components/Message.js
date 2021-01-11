b6dac44587fc623235ce543fdeea37f05a7fb2ad
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-01-11 16:16:58 +0000

    Update Message.js

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,12 +1,7 @@
 import React from "react"
-import { Alert } from "react-bootstrap"
 
 const Message = ({ className, children }) => {
 return <div className={className}>{children}</div>
-}
-
-Message.defaultProps = {
-    variant: "info"
 }
  
 export default Message
