536cf25975343ccf5cf7fb61f008a0e3b453b2d6
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-02-10 15:15:52 +0000

    Update contactForm.js

--- frontend/src/container/contact/contactForm.js
+++ frontend/src/container/contact/contactForm.js
@@ -1,1 +1,30 @@
+const content =  {
+    inputs: [
+        {
+            form: "text",
+            name: "username",
+            type: "text",
+            placeholder: "Your Name (Required)"
+        },
+        {
+            form: "text",
+            name: "email",
+            type: "email",
+            placeholder: "Your Email (Required)"
+        },
+        {
+            form: "text",
+            name: "subject",
+            type: "text",
+            placeholder: "Subject"
+        },
+        {
+            form: "textarea",
+            name: "message",
+            type: "text",
+            placeholder: "Your Message"
+        },
+    ]
+}
 
+export default content;
