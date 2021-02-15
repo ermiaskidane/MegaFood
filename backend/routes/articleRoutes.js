b2f05b0f4b0fc00e4efe5c24fb54e64daed4e49c
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-02-15 15:49:04 +0000

    Update articleRoutes.js

--- backend/routes/articleRoutes.js
+++ backend/routes/articleRoutes.js
@@ -1,1 +1,23 @@
+import express from "express"
+import {
+    getArticle,
+    postArticle,
+    editArticle,
+    updateArticle,
+    deleteArticle
+   } from "../controllers/articleController.js"
 
+const router = express.Router()
+
+router.get("/all", getArticle)
+
+router.post("/", postArticle)
+
+router.get("/edit/:id", editArticle)
+
+router.put("/:id", updateArticle)
+
+router.delete("/:id", deleteArticle)
+
+
+export default router
